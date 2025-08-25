3ab931004143774febfce4d804e05a518fb58624
Bambam <bam.ksnru@gmail.com>
2025-08-25 15:07:46 +0700

    feat: rendering list + router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import UsersList from "./pages/UserList";
+import "./index.css"; // TODO: ตรวจสอบว่า TailwindCSS ถูก import แล้ว
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* TODO: กำหนด props name ของ Home */}
+        <Route path="/" element={<Home name="Alice" />} />
+        <Route path="/users" element={<UsersList />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
