4945b3926f7ef624c04619d3153985af9295fa92
Bambam <bam.ksnru@gmail.com>
2025-08-25 11:30:49 +0700

    feat: ex.1 greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import "./App.css";
+// App.tsx
+import Greeting from "./Greeting";
 
 function App() {
-  // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return (
+    <div>
+      <Greeting name="Alice" age={25} />
+    </div>
+  );
 }
 
 export default App;
--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -1,17 +1,20 @@
 // Exercise1.tsx
-import { useState } from "react";
+import React, { useState } from "react";
 
-// TODO1:  ประกาศ Type ด้วย type หรือ inerface
+// TODO1:  ประกาศ Type ด้วย type หรือ interface
+interface GreetingProps {
+  name: string;
+  age: number;
+}
 
 // TODO2: กำหนด Type ให้ props
-const Greeting = (props) => {
+const Greeting = ({ name, age }: GreetingProps) => {
   // TODO3: กำหนด Type ให้ useState
-  const [count, setCount] = useState();
-
-  return (
+  const [count, setCount] = useState<number>(0);
+ return (
     <div>
       <h1>
-        Hello {props.name}, you are {props.age} years old 👋
+        Hello {name}, you are {age} years old 👋
       </h1>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click Me</button>
